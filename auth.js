eed8e43cae585a3368f9ca154bd5a5f6f3c9b68c
ELuchini <168753402+ELuchini@users.noreply.github.com>
2025-08-22 17:33:25 +0000

    (16 ) Implementation of Social Authentication III - OK

--- auth.js
+++ auth.js
@@ -2,9 +2,7 @@ const bcrypt = require("bcrypt");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const { ObjectID } = require("mongodb");
-const GitHubStrategy = require('passport-github').Strategy;
-
-
+const GitHubStrategy = require("passport-github").Strategy;
 
 module.exports = function (app, myDataBase) {
   /*  passport.use(
@@ -19,16 +17,46 @@ module.exports = function (app, myDataBase) {
     }),
   ); */
 
-  passport.use(new GitHubStrategy({
-      clientID: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: 'https://bc0febad-a10d-4a33-8ab2-89690f7ecf93-00-2g3np9a8t5613.spock.replit.dev/', /*INSERT CALLBACK URL ENTERED INTO GITHUB HERE*/
-    },
-      function(accessToken, refreshToken, profile, cb) {
+  passport.use(
+    new GitHubStrategy(
+      {
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL:
+          "https://bc0febad-a10d-4a33-8ab2-89690f7ecf93-00-2g3np9a8t5613.spock.replit.dev/" /*INSERT CALLBACK URL ENTERED INTO GITHUB HERE*/,
+      },
+      function (accessToken, refreshToken, profile, cb) {
         console.log(profile);
         //Database logic here with callback containing your user object
-      }
-    ));
+        myDataBase.findOneAndUpdate(
+          { id: profile.id },
+          {
+            $setOnInsert: {
+              id: profile.id,
+              username: profile.username,
+              name: profile.displayName || "John Doe",
+              photo: profile.photos[0].value || "",
+              email: Array.isArray(profile.emails)
+                ? profile.emails[0].value
+                : "No public email",
+              created_on: new Date(),
+              provider: profile.provider || "",
+            },
+            $set: {
+              last_login: new Date(),
+            },
+            $inc: {
+              login_count: 1,
+            },
+          },
+          { upsert: true, new: true },
+          (err, doc) => {
+            return cb(null, doc.value);
+          },
+        );
+      },
+    ),
+  );
 
   passport.use(
     new LocalStrategy((username, password, done) => {
