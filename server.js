ce9fcc52370c2f8d4d907063fe43e705795a45cb
ELuchini <168753402+ELuchini@users.noreply.github.com>
2025-08-22 03:15:55 +0000

    (7) How to Use Passport Strategies - Ok

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const session = require("express-session");
 const passport = require("passport");
 const { ObjectID } = require("mongodb");
-const LocalStrategy = require('passport-local');
+const LocalStrategy = require("passport-local");
 
 const app = express();
 
@@ -33,21 +33,37 @@ app.use(express.urlencoded({ extended: true }));
 myDB(async (client) => {
   const myDataBase = await client.db("database").collection("users");
 
-  passport.use(new LocalStrategy((username, password, done) => {
-    myDataBase.findOne({ username: username }, (err, user) => {
-      console.log(`User ${username} attempted to log in.`);
-      if (err) return done(err);
-      if (!user) return done(null, false);
-      if (password !== user.password) return done(null, false);
-      return done(null, user);
-    });
-  }));
+  passport.use(
+    new LocalStrategy((username, password, done) => {
+      myDataBase.findOne({ username: username }, (err, user) => {
+        console.log(`User ${username} attempted to log in.`);
+        if (err) return done(err);
+        if (!user) return done(null, false);
+        if (password !== user.password) return done(null, false);
+        return done(null, user);
+      });
+    }),
+  );
 
   app.route("/").get((req, res) => {
     res.render("index", {
       title: "Connected to Database",
       message: "Please log in",
+      showLogin: true,
     });
+  });
+
+  app
+    .route("/login")
+    .post(
+      passport.authenticate("local", { failureRedirect: "/" }),
+      (req, res) => {
+        res.redirect("/profile");
+      },
+    );
+
+  app.route("/profile").get((req, res) => {
+    res.render("profile");
   });
 
   passport.serializeUser((user, done) => {
